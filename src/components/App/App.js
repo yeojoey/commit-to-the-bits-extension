a676b923bd1aee1e2f2eb815fb3b47000d59aca3
ANDREW\joeyy <joeyy@andrew.cmu.edu>
2019-02-21 16:01:01 -0500

    Wait for auth to complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,6 +54,18 @@ class App extends Component {
                       return {finishedLoading:true}
                   })
               }
+
+              callApi = async () => {
+                console.log(this.Authentication.state.token);
+                const response = await fetch("/api/getScream", {
+                  method: "GET",
+                  headers: {
+                    "authorization": this.Authentication.state.token
+                  }
+                });
+              const body = await response.json();
+              if (response.status !== 200) throw Error(body.message);
+              this.setState(body);
           })
 
           this.twitch.listen("broadcast", (target, contentType, message) => {
@@ -82,18 +94,7 @@ class App extends Component {
         .catch(err => console.log(err));
   }
 
-  callApi = async () => {
-    console.log(this.Authentication.state.token);
-    const response = await fetch("/api/getScream", {
-      method: "GET",
-      headers: {
-        "authorization": this.Authentication.state.token
-      }
-    });
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    this.setState(body);
-    return body;
+
   }
 
   componentWillUnmount(){
