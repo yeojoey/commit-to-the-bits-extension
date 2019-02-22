28b4e5c75927b469ca8faf9de74904fbe7a3e8ca
ANDREW\joeyy <joeyy@andrew.cmu.edu>
2019-02-22 14:21:05 -0500

    Remove fake comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -186,11 +186,6 @@ class App extends Component {
 
                   {this.state.botState.isVoting ? <Voting options={this.state.botState.options} /> : <b>{this.state.botState.finalWord}</b>}
 
-                      // <p>Hello world!</p>
-                      // <p>My token is: {this.Authentication.state.token}</p>
-                      // <p>My opaque ID is {this.Authentication.getOpaqueId()}.</p>
-                      // <div>{this.Authentication.isModerator() ? <p>I am currently a mod, and here's a special mod button <input value='mod button' type='button'/></p>  : 'I am currently not a mod.'}</div>
-                      // <p>I have {this.Authentication.hasSharedId() ? `shared my ID, and my user_id is ${this.Authentication.getUserId()}` : 'not shared my ID'}.</p>
                       <p>{this.state.textToDisplay}</p>
                       <form onSubmit={this.handleSubmit}>
                         <button type="submit">Scream</button>
