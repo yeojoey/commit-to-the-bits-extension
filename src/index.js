32b99b38221247b5c29ce8c672c5a77e6bc288c8
ANDREW\joeyy <joeyy@andrew.cmu.edu>
2019-02-22 17:49:59 -0500

    Make bg transparent

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App/App"
 
-import logo from './logo.svg';
-
 ReactDOM.render(
   <App />,
   document.getElementById("root")
