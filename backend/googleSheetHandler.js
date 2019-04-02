324fa841a0cc82e524665045639cc74cddfbbbba
CharliePRamsey <parkerrmsy@gmail.com>
2019-04-02 13:00:05 -0400

    remove drive stuff for later

--- backend/googleSheetHandler.js
+++ backend/googleSheetHandler.js
@@ -69,24 +69,24 @@ const GoogSheet = class GoogleSheetHandler
   }
 
 
-  listFiles(auth) {
-    const drive = google.drive({version: 'v3', auth});
-    drive.files.list({
-      pageSize: 10,
-      fields: 'nextPageToken, files(id, name)',
-    }, (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err);
-      const files = res.data.files;
-      if (files.length) {
-        console.log('Files:');
-        files.map((file) => {
-          console.log(`${file.name} (${file.id})`);
-        });
-      } else {
-        console.log('No files found.');
-      }
-    });
-  }
+  // listFiles(auth) {
+  //   const drive = google.drive({version: 'v3', auth});
+  //   drive.files.list({
+  //     pageSize: 10,
+  //     fields: 'nextPageToken, files(id, name)',
+  //   }, (err, res) => {
+  //     if (err) return console.log('The API returned an error: ' + err);
+  //     const files = res.data.files;
+  //     if (files.length) {
+  //       console.log('Files:');
+  //       files.map((file) => {
+  //         console.log(`${file.name} (${file.id})`);
+  //       });
+  //     } else {
+  //       console.log('No files found.');
+  //     }
+  //   });
+  // }
 
   readWhiteListedUsers(auth, self)
   {
