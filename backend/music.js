71cc18f07daaaeec1c62d2fb0f8fd84d93b6699a
CharliePRamsey <parkerrmsy@gmail.com>
2019-04-03 17:28:06 -0400

    request promise

--- backend/music.js
+++ backend/music.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request = require('request-promise');
 
 const moods = ["Action", "Heavy Metal", "Marriage", "Romance", "Spooky", "Very Sad", "Wild West"];
 const Muse = class Music
