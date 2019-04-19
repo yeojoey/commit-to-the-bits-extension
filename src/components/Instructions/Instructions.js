a4e3d37c9eb51a5cec8bbcc42b745236a5f705fd
ANDREW\joeyy <joeyy@andrew.cmu.edu>
2019-04-19 11:01:21 -0400

    Update instructions

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -42,18 +42,38 @@ class Instructions extends Component {
 
           <p>Have fun making this traveller's day more difficult!</p>
         </Modal.Body>
-        :
+        : this.props.currentGame === "Courtroom" ?
         <Modal.Body>
-          <p>In Untitled Courtroom Game, performers and audience members work together to create a scene.  One performer
+          <p>In Discord in the Court, performers and audience members work together to create a scene.  One performer
           takes on the role of a prosecutor while the other is a defendant.  The prosecutor is trying to convince the
           judge that the defendant is guilty and does so by bringing in witnesses.</p>
 
-          <p>Witnesses are pulled from the audience from a queue and are given a role by the prosecutor (e.g. “Next we’ll
-            hear from the owner of the shop where the crime took place.”)  Witnesses give improvised statements.  These
-            statements should be completely random and have no (or little) connection to previous statements.  The defendant
-            takes those statements and tries to explain why they did so, making up their crime in the process.</p>
+          <p>Witnesses are pulled from the audience from a queue and are given a role by the prosecutor (e.g. “Next
+            we’ll hear from the owner of the shop where the crime took place.”)  Witnesses give improvised statements.
+            These statements should be completely random and have no (or little) connection to previous statements.
+            The defendant takes those statements and tries to explain why they did so, making up their crime in the
+             process.</p>
 
           <p>To play, audience members should enter their Discord user name into the box on the overlay.</p>
+        </Modal.Body>
+        : this.props.currentGame === "Music" ?
+        <Modal.Body>
+          <p>In Music to my Peers, performers act out a scene based on a suggestion from the audience.  At some point
+          in the scene, an audience member will be selected to be the DJ.  The DJ gets to pick several songs that will
+          play during the scene, changing the mood of the performers and the overall feel of the scene. </p>
+
+          <p>If you’d like to be the DJ, just click the “Pick Me” button and you’ll be added to the list!  DJs are selected
+          randomly from the list.  Once you’re the DJ, you can pick three songs by clicking the buttons with song genres.
+           Once the scene ends, a new DJ will be picked and a new scene will start. </p>
+        </Modal.Body>
+        :
+        <Modal.Body>
+          <p>In Guesstination Unknown, audience members can input three suggestions - a noun, a verb, and a location.
+          One of each of these is picked to create a scenario (like “The Joker washing a goat in Paris), but the audience
+          doesn’t know what that final scenario is.  A single improv performer will go on stage and perform a scene based
+          on that scenario.  The audience must guess the three suggestions based on what they see.</p>
+
+          <p>Suggestions are input on the overlay while guesses are done <b>through chat.</b></p>
         </Modal.Body>
         }
         <Modal.Footer>
